45b8c340ff1d1d30df2970ab74a3bc4e5e30ebe6
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2020-11-17 11:02:59 +0100

    Added password

---
+++ PAMU_CDS/Actions/UpdateRecordAction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using PAMU_CDS.Auxiliary;
+using Parser.ExpressionParser;
+using Parser.FlowParser.ActionExecutors;
+
+namespace PAMU_CDS.Actions
+{
+    public class CreateRecordAction : OpenApiConnectionActionExecutorBase
+    {
+        private readonly IOrganizationService _organizationService;
+
+        public CreateRecordAction(ExpressionEngine expressionEngine, IOrganizationService organizationService) : base(
+            expressionEngine)
+        {
+            _organizationService = organizationService ?? throw new ArgumentNullException(nameof(organizationService));
+        }
+
+        public override Task<ActionResult> Execute()
+        {
+
+            var entity = new Entity();
+            entity = entity.CreateEntityFromParameters(Parameters);
+
+            try
+            {
+                _organizationService.Create(entity);
+            }
+            catch (InvalidPluginExecutionException)
+            {
+                // We need to do some experiments on how the error handling works. Take a look at one of your customers.
+                return Task.FromResult(new ActionResult {ActionStatus = ActionStatus.Failed});
+            }
+
+            return Task.FromResult(new ActionResult {ActionStatus = ActionStatus.Succeeded});
+        }
+    }
+}
