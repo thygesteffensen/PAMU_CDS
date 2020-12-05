820e9e5726a0a5d2fbaae815b14ba7c04a2f97e2
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2020-12-05 10:17:25 +0100

    feat: Entity helper as extension
    I have added two extensions to Entity to help conversion between Entity and ValueContainer

--- PAMU_CDS/Auxiliary/EntityExtension.cs
+++ PAMU_CDS/Auxiliary/EntityExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xrm.Sdk;
 using Parser.ExpressionParser;
@@ -8,15 +9,17 @@ namespace PAMU_CDS.Auxiliary
     public static class EntityExtension
     {
         public static Entity CreateEntityFromParameters(this Entity entity,
-            Dictionary<string, ValueContainer> parameters)
+            ValueContainer parameters)
         {
-            entity.LogicalName = parameters["entityName"].GetValue<string>();
+            // Dynamics API uses plural names for entites/tables, which isn't the name used as logical names...
+            var entityName = parameters["entityName"].GetValue<string>();
+            entity.LogicalName = entityName.Substring(0, entityName.Length - 1); 
 
-            foreach (KeyValuePair<string, ValueContainer> keyValuePair in parameters.Where(kv =>
-                kv.Key.StartsWith("item")))
+            foreach (KeyValuePair<string, ValueContainer> keyValuePair in parameters["item"]
+                .GetValue<Dictionary<string, ValueContainer>>())
             {
                 // TODO: Figure out how to determine which value is expected.
-                entity.Attributes[keyValuePair.Key.Replace("item/", "")] = keyValuePair.Value.GetValue<string>();
+                entity.Attributes[keyValuePair.Key] = keyValuePair.Value.GetValue<string>();
             }
 
             return entity;
@@ -27,12 +30,62 @@ namespace PAMU_CDS.Auxiliary
             var triggerOutputs = new Dictionary<string, ValueContainer>();
             foreach (var keyValuePair in entity.Attributes)
             {
-                // TODO: Verify this works!
-                // TODO: Look into the processed fields, starting with '_'
-                triggerOutputs[keyValuePair.Key] = new ValueContainer(keyValuePair.Value.ToString(), true);
+                AddObjectToValueContainer(triggerOutputs, keyValuePair);
             }
 
-            return new ValueContainer(triggerOutputs);
+            return new ValueContainer(new Dictionary<string, ValueContainer>
+                {{"body", new ValueContainer(triggerOutputs)}});
+        }
+
+        private static void AddObjectToValueContainer(this IDictionary<string, ValueContainer> dict,
+            KeyValuePair<string, object> kvp)
+        {
+            switch (kvp.Value)
+            {
+                case EntityReference entityReference:
+                    // This is the primary column of the related entity's value
+                    // dict[$"_{kvp.Key}_value@OData.Community.Display.V1.FormattedValue"] = new ValueContainer(entityReference.Id.ToString());
+                    dict[$"_{kvp.Key}_value@Microsoft.Dynamics.CRM.associatednavigationproperty"] =
+                        new ValueContainer(entityReference.Id.ToString());
+                    dict[$"_{kvp.Key}_value@Microsoft.Dynamics.CRM.lookuplogicalname"] =
+                        new ValueContainer(entityReference.LogicalName);
+                    dict[$"_{kvp.Key}_value@odata.type"] = new ValueContainer("#Guid");
+                    dict[$"_{kvp.Key}_value"] = new ValueContainer(entityReference.Id.ToString());
+                    break;
+                case string s:
+                    dict[kvp.Key] = new ValueContainer(s);
+                    break;
+                case int i:
+                    dict[kvp.Key] = new ValueContainer(i);
+                    break;
+                case double d:
+                    dict[kvp.Key] = new ValueContainer((float) d);
+                    break;
+                case Guid guid:
+                    dict[$"{kvp.Key}@odata.type"] = new ValueContainer("#Guid");
+                    dict[kvp.Key] = new ValueContainer(guid.ToString());
+                    break;
+                case OptionSetValue optionSetValue:
+                    dict[$"{kvp.Key}@OData.Community.Display.V1.FormattedValue"] =
+                        new ValueContainer(
+                            "<label not yet present>"); // The options set value. We'll need the context...
+                    dict[kvp.Key] = new ValueContainer(optionSetValue.Value);
+                    break;
+                case null:
+                    break;
+                case bool b:
+                    dict[kvp.Key] = new ValueContainer(b);
+                    break;
+                // TODO: Figure out how Mockup handles Date and Datetime offset? see birthdate and modifiedon for reference
+                case DateTime dateTime:
+                    dict[$"{kvp.Key}"] =
+                        dict[$"{kvp.Key}@odata.type"] = new ValueContainer("#Date");
+                    dict[kvp.Key] = new ValueContainer(dateTime.ToString());
+                    break;
+                default:
+                    throw new NotImplementedException();
+                // throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
