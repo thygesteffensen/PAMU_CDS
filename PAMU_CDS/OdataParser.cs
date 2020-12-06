c19d10220efc45e47ac4b9f6a85e1d3e1ea75c74
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2020-12-06 10:51:43 +0100

    Odata version 2

--- PAMU_CDS/OdataParser.cs
+++ PAMU_CDS/OdataParser.cs
@@ -5,7 +5,7 @@ namespace PAMU_CDS
 {
     public class OdataParser
     {
-        private readonly Parser<OData> _inputParser;
+        private readonly Parser<Value[]> _values;
 
         public OdataParser()
         {
@@ -14,50 +14,40 @@ namespace PAMU_CDS
                     Parse.Char('=')).Or(
                     Parse.Char('(')).Or(
                     Parse.Char(')')).Or(
+                    Parse.Char(';')).Or(
                     Parse.Char(','));
 
             var simpleString = Parse.AnyChar.Except(except).AtLeastOnce().Text();
 
-            var oDataValues =
+            var parameters =
                 from paras in (
                     from name in simpleString.Contained(Parse.Char('$'), Parse.Char('='))
                     from props in simpleString.DelimitedBy(Parse.Char(','))
-                    select new ODatavalue {Name = name, Properties = props.ToArray()}
-                ).DelimitedBy(Parse.Char(','))
+                    select new ODataValue {Name = name, Properties = props.ToArray()}
+                ).DelimitedBy(Parse.Char(';'))
                 select paras.ToArray();
 
-            var options =
-                from vals in (
+            _values = from vals in (
                     from optionName in simpleString
-                    from paras in oDataValues.Contained(Parse.Char('('), Parse.Char(')')).Optional()
-                    select new ODataOption {OptionName = optionName, Parameters = paras.IsEmpty ? null : paras.Get()}
+                    from paras in parameters.Contained(Parse.Char('('), Parse.Char(')')).Optional()
+                    select new Value {Option = optionName, Parameters = paras.IsEmpty ? null : paras.Get()}
                 ).DelimitedBy(Parse.Char(','))
                 select vals.ToArray();
-
-            _inputParser = from name in simpleString.Contained(Parse.Char('$'), Parse.Char('='))
-                from vals in options
-                select new OData {Name = name, Values = vals};
         }
 
-        public OData Get(string input)
+        public Value[] Get(string input)
         {
-            return _inputParser.Parse(input);
+            return _values.Parse(input);
         }
     }
 
-    public class OData
+    public class Value
     {
-        public string Name { get; set; }
-        public ODataOption[] Values { get; set; }
+        public string Option { get; set; }
+        public ODataValue[] Parameters { get; set; }
     }
 
-    public class ODataOption
-    {
-        public string OptionName { get; set; }
-        public ODatavalue[] Parameters { get; set; }
-    }
-
-    public class ODatavalue
+    public class ODataValue
     {
         public string Name { get; set; }
         public string[] Properties { get; set; }
--- Test/ODataParserTest.cs
+++ Test/ODataParserTest.cs
@@ -9,50 +9,62 @@ namespace Test
         [TestMethod]
         public void TestOdataParser()
         {
-            var selectQuery = "$expand=primarycontactid($select=contactid,fullname)";
+            const string selectQuery = "primarycontactid($select=contactid,fullname)";
 
             var oDataParser = new OdataParser();
 
             var oData = oDataParser.Get(selectQuery);
 
-            Assert.AreEqual("expand", oData.Name);
-            Assert.AreEqual(1, oData.Values.Length);
-            Assert.AreEqual("primarycontactid", oData.Values[0].OptionName);
-            Assert.AreEqual(1, oData.Values[0].Parameters.Length);
-            Assert.AreEqual("select", oData.Values[0].Parameters[0].Name);
-            Assert.AreEqual(2, oData.Values[0].Parameters[0].Properties.Length);
-            Assert.AreEqual("contactid", oData.Values[0].Parameters[0].Properties[0]);
-            Assert.AreEqual("fullname", oData.Values[0].Parameters[0].Properties[1]);
+            Assert.AreEqual(1, oData.Length);
+            Assert.AreEqual("primarycontactid", oData[0].Option);
+            Assert.AreEqual(1, oData[0].Parameters.Length);
+            Assert.AreEqual("select", oData[0].Parameters[0].Name);
+            Assert.AreEqual(2, oData[0].Parameters[0].Properties.Length);
+            Assert.AreEqual("contactid", oData[0].Parameters[0].Properties[0]);
+            Assert.AreEqual("fullname", oData[0].Parameters[0].Properties[1]);
         }
+        
+        [TestMethod]
+        public void TestOdataParser1()
+        {
+            const string selectQuery = "primarycontactid($select=contactid,fullname;$orderby=createdon asc;$filter=endswith(subject,'1'))";
+            const string selectQuery = "$expand=primarycontactid($filter=endswith(subject,'1'))";
 
+            var oDataParser = new OdataParser();
+
+            var oData = oDataParser.Get(selectQuery);
+
+            Assert.AreEqual(1, oData.Length);
+            Assert.AreEqual("primarycontactid", oData[0].Option);
+            Assert.AreEqual(1, oData[0].Parameters.Length);
+            Assert.AreEqual("select", oData[0].Parameters[0].Name);
+            Assert.AreEqual(2, oData[0].Parameters[0].Properties.Length);
+            Assert.AreEqual("contactid", oData[0].Parameters[0].Properties[0]);
+            Assert.AreEqual("fullname", oData[0].Parameters[0].Properties[1]);
+        }
+        
         [TestMethod]
         public void TestOdataParser2()
         {
-            var selectQuery = "$select=primarycontactid,option2";
+            const string selectQuery = "dca_contact_dca_socialsecurityno";
 
             var oDataParser = new OdataParser();
 
             var oData = oDataParser.Get(selectQuery);
 
-            Assert.AreEqual("select", oData.Name);
-            Assert.AreEqual(2, oData.Values.Length);
-            Assert.AreEqual("primarycontactid", oData.Values[0].OptionName);
-            Assert.AreEqual("option2", oData.Values[1].OptionName);
+            Assert.AreEqual(1, oData.Length);
         }
         
         [TestMethod]
         public void TestOdataParser3()
         {
-            var selectQuery = "dca_contact_dca_socialsecurityno";
+            const string selectQuery = "relationship_1,relationship_2($select=name)";
 
             var oDataParser = new OdataParser();
 
             var oData = oDataParser.Get(selectQuery);
 
-            Assert.AreEqual("select", oData.Name);
-            Assert.AreEqual(2, oData.Values.Length);
-            Assert.AreEqual("primarycontactid", oData.Values[0].OptionName);
-            Assert.AreEqual("option2", oData.Values[1].OptionName);
+            Assert.AreEqual(1, oData.Length);
         }
         
     }
