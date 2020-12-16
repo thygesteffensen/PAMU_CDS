aec62a4892798a824bcbd0c72c58c2df1810a076
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2020-12-16 16:22:44 +0100

    Version 2

--- PAMU_CDS/Auxiliary/OdataFilter.cs
+++ PAMU_CDS/Auxiliary/OdataFilter.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using Sprache;
 
@@ -6,103 +8,202 @@ namespace PAMU_CDS.Auxiliary
 {
     public class OdataFilter
     {
-        Parser<ConditionExpression> stm;
-        Parser<Temp1> cond;
+        Parser<ParsedFilter> cond1;
 
-        public OdataFilter()
+        private static readonly Parser<char> Except =
+            Parse.Char('$').Or(
+                Parse.Char('=')).Or(
+                Parse.Char('(')).Or(
+                Parse.Char(')')).Or(
+                Parse.Char(';')).Or(
+                Parse.Char(' ')).Or(
+                Parse.Char('\'')).Or(
+                Parse.Char(','));
+
+        private static readonly Parser<IOdata> SimpleString =
+            Parse.AnyChar.Except(Except).AtLeastOnce().Text().Select(x => new Constant(x));
+
+        private static readonly Parser<ConditionOperator> Equal = Parse.String("eq").Return(ConditionOperator.Equal);
+
+        private static readonly Parser<ConditionOperator> LessThan =
+            Parse.String("lt").Return(ConditionOperator.LessThan);
+
+        private static readonly Parser<ConditionOperator> GreaterThan =
+            Parse.String("gt").Return(ConditionOperator.GreaterThan);
+
+        private static readonly Parser<ConditionOperator> GreaterOrEqual =
+            Parse.String("ge").Return(ConditionOperator.GreaterEqual);
+
+        private static readonly Parser<ConditionOperator> LessOrEqual =
+            Parse.String("le").Return(ConditionOperator.LessEqual);
+
+        private static readonly Parser<ConditionOperator> NotEqual =
+            Parse.String("ne").Return(ConditionOperator.NotEqual);
+
+        private static readonly Parser<ConditionOperator> Operators =
+            Equal.Or(LessThan).Or(GreaterThan).Or(GreaterOrEqual).Or(LessOrEqual).Or(NotEqual);
+
+        private static readonly Parser<LogicalOperator> And = Parse.String("and").Return(LogicalOperator.And);
+        private static readonly Parser<LogicalOperator> Or = Parse.String("or").Return(LogicalOperator.Or);
+
+        private static readonly Parser<LogicalOperator> LOperators = And.Or(Or);
+
+        private static readonly Parser<char> Space = Parse.Char(' ');
+        private static readonly Parser<char> Quote = Parse.Char('\'');
+        private static readonly Parser<char> OpenP = Parse.Char('(');
+        private static readonly Parser<char> CloseP = Parse.Char(')');
+
+        private static readonly Parser<IOdata> Func =
+            from funcName in SimpleString
+            from paras in SimpleString.Contained(OpenP, CloseP)
+            select new Function(funcName, paras);
+
+        private static readonly Parser<IOdata> Value =
+            Func.Or(SimpleString.Contained(Quote, Quote));
+
+        private static readonly Parser<IOdata> Stm =
+            Func.Or(Parse.Ref(() => OrGroup).Contained(OpenP, CloseP)).Or(
+                from attr in SimpleString
+                from op in Operators.Contained(Space, Space)
+                from val in Value
+                select new Statement((Constant) attr, op, val)
+            );
+
+        private static readonly Parser<IOdata> AndGroup =
+            from stmt in Stm
+            from groups in (
+                from o in And
+                from a1 in Stm
+                select new Group(o, a1)).Many()
+            select new Group(stmt, groups);
+
+        private static readonly Parser<IOdata> OrGroup =
+            from andGroup in AndGroup
+            from groups in (
+                from o in Or
+                from a1 in AndGroup
+                select new Group(o, a1)).Many()
+            select new Group(andGroup, groups);
+
+        public FilterExpression OdataToFilterExpression(string input)
         {
-            var except =
-                Parse.Char('$').Or(
-                    Parse.Char('=')).Or(
-                    Parse.Char('(')).Or(
-                    Parse.Char(')')).Or(
-                    Parse.Char(';')).Or(
-                    Parse.Char(' ')).Or(
-                    Parse.Char('\'')).Or(
-                    Parse.Char(','));
-
-            var simpleString = Parse.AnyChar.Except(except).AtLeastOnce().Text();
-
-            var equal = Parse.String("eq").Return(ConditionOperator.Equal);
-            var lessThan = Parse.String("lt").Return(ConditionOperator.LessThan);
-            var greaterThan = Parse.String("gt").Return(ConditionOperator.GreaterThan);
-            var greaterOrEqual = Parse.String("ge").Return(ConditionOperator.GreaterEqual);
-            var lessOrEqual = Parse.String("le").Return(ConditionOperator.LessEqual);
-            var notEqual = Parse.String("ne").Return(ConditionOperator.NotEqual);
-
-            var operators = equal.Or(lessThan).Or(greaterThan).Or(greaterOrEqual).Or(lessOrEqual).Or(notEqual);
-
-            var and = Parse.String("and").Return(LogicalOperator.And);
-            var or = Parse.String("or").Return(LogicalOperator.Or);
-
-            var lOperators = and.Or(or);
-
-            var str = Parse.AnyChar.AtLeastOnce().Text();
-
-            var space = Parse.Char(' ');
-            var quote = Parse.Char('\'');
-
-            stm = from attr in simpleString
-                from op in operators.Contained(space, space)
-                from val in simpleString.Contained(quote, quote)
-                select new ConditionExpression(attr, op, val);
-
-            cond = from t in stm
-                from dep in (
-                    from op in lOperators
-                    from t1 in stm
-                    select new Temp(op, t1)).Many()
-                select new Temp1(t, dep);
-        }
-
-        public ConditionExpression ParseInput(string input)
-        {
-            return stm.Parse(input);
-        }
-
-        public FilterExpression ParseToFiler(string input)
-        {
+            var t = (Group) OrGroup.Parse(input);
 
 
             return null;
         }
+    }
 
-        public void T()
+    public interface IOdata
+    {
+    };
+
+    public class Statement : IOdata
+    {
+        private readonly Constant _attr;
+        private readonly ConditionOperator _op;
+        private readonly IOdata _val;
+
+        public Statement(Constant attr, ConditionOperator op, IOdata val)
         {
-            var exp = new FilterExpression()
+            _attr = attr;
+            _op = op;
+            _val = val;
+        }
+
+        public ConditionExpression ToConditionExpression()
+        {
+            return new ConditionExpression(_attr.Value(), _op, _val);
+        }
+    }
+
+    public class Function : IOdata
+    {
+        private readonly IOdata _funcName;
+        private readonly IOdata _paras;
+
+        public Function(IOdata funcName, IOdata paras)
+        {
+            _funcName = funcName;
+            _paras = paras;
+        }
+    }
+
+    public class Group : IOdata
+    {
+        public LogicalOperator LogicalOperator { get; }
+        public readonly IOdata Grp;
+        public readonly IOdata Stmt;
+        public readonly IEnumerable<Group> SameGrp;
+
+        public Group(LogicalOperator logicalOperator, IOdata grp)
+        {
+            LogicalOperator = logicalOperator;
+            Grp = grp;
+        }
+
+        public Group(IOdata stmt, IEnumerable<Group> sameGrp)
+        {
+            Stmt = stmt;
+            SameGrp = sameGrp;
+        }
+
+        public FilterExpression ToFilerExpression()
+        {
+            var filter = new FilterExpression()
             {
-                FilterOperator = LogicalOperator.And,
+                FilterOperator = LogicalOperator,
                 Conditions =
                 {
-                   new ConditionExpression(), 
-                   new ConditionExpression(),
-                   
-                }
+                    Stmt.ToConditionExpression()
+                },
             };
-            
-            var q = new QueryExpression()
+
+            foreach (var group in SameGrp)
             {
-                
-            };
-            q.Criteria.AddFilter(new FilterExpression());
-            q.Criteria.AddFilter(new FilterExpression());
+                filter.Filters.Add(group.ToFilerExpression());
+            }
 
-            var f = new ConditionExpression();
+            return filter;
         }
     }
 
-    public class Temp1
+    public class Constant : IOdata
     {
-        public Temp1(ConditionExpression conditionExpression, IEnumerable<Temp> dep)
+        private readonly string _s;
+
+        public Constant(string s)
         {
+            _s = s;
+        }
+
+        public string Value()
+        {
+            return _s;
         }
     }
 
-    public class Temp
+    public class ParsedFilter
     {
-        public Temp(LogicalOperator op, ConditionExpression t1)
+        public ConditionExpression ConditionExpression { get; }
+        public IEnumerable<Additional> Additional { get; }
+
+        public ParsedFilter(ConditionExpression conditionExpression, IEnumerable<Additional> additional)
         {
-            
+            ConditionExpression = conditionExpression;
+            Additional = additional;
+        }
+    }
+
+    public class Additional
+    {
+        public LogicalOperator Op { get; }
+        public ConditionExpression ConditionExpression { get; }
+
+        public Additional(LogicalOperator op, ConditionExpression conditionExpression)
+        {
+            Op = op;
+            ConditionExpression = conditionExpression;
         }
     }
 }
---
+++ Test/OdataFilterTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk.Query;
+using PAMU_CDS.Auxiliary;
+
+namespace Test
+{
+    [TestClass]
+    public class OdataFilterTest
+    {
+        [TestMethod]
+        public void TestOneCondition()
+        {
+            var input = "fullname eq 'Hej'";
+            
+            var parser = new OdataFilter();
+
+            var filter = parser.OdataToFilterExpression(input);
+            var cond = filter.Conditions.First();
+            
+            Assert.AreEqual(ConditionOperator.Equal, cond.Operator);
+            Assert.AreEqual("fullname", cond.AttributeName);
+            Assert.AreEqual("Hej", cond.Values.First());
+        }
+        
+        [TestMethod]
+        public void TestAndCondition()
+        {
+            var input = "fullname eq 'Hej' and firstname eq 'Hej'";
+            
+            var parser = new OdataFilter();
+
+            var filter = parser.OdataToFilterExpression(input);
+            
+            Assert.AreEqual(LogicalOperator.And ,filter.FilterOperator);
+            Assert.AreEqual(2, filter.Conditions.Count);
+            
+            var condition1 = filter.Conditions.First();
+            
+            Assert.AreEqual(ConditionOperator.Equal, condition1.Operator);
+            Assert.AreEqual("fullname", condition1.AttributeName);
+            Assert.AreEqual("Hej", condition1.Values.First());
+            
+            var condition2 = filter.Conditions.First();
+            
+            Assert.AreEqual(ConditionOperator.Equal, condition2.Operator);
+            Assert.AreEqual("firstname", condition2.AttributeName);
+            Assert.AreEqual("Hej", condition2.Values.First());
+        }
+    }
+}
