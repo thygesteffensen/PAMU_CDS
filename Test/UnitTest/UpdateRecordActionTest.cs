262c1b41a838e1d1034b63005a40e241ec385cb0
Thyge Skoedt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2021-01-13 19:34:28 +0100

    feat: trigger release

--- Test/UnitTest/UpdateRecordActionTest.cs
+++ Test/UnitTest/UpdateRecordActionTest.cs
@@ -71,6 +71,7 @@ namespace Test.UnitTest
             Assert.IsTrue(_entity.Attributes.ContainsKey("address1_line1"));
             // Assert.AreEqual("Upsert", updateRequest.RequestName);
             
+            
             Assert.IsNotNull(outputActionName);
             Assert.AreEqual("UpdateAccount", outputActionName);
 
